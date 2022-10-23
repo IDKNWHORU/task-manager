273205d0def62aca22965104c9c0162ffda040d0
jrh <junho9341@gmail.com>
2022-10-23 21:49:54 +0900

    add dynamic data render

--- index.js
+++ index.js
@@ -13,9 +13,27 @@ const state = {
   }]
 }
 
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const main = document.body;
+    const newMain = registry.renderRoot(main, state);
+    main.replaceWith(newMain);
+  })
+}
 
-window.requestAnimationFrame(() => {
-  const main = document.body;
-  const newMain = registry.renderRoot(main, state);
-  main.replaceWith(newMain);
-})
+window.setInterval(() => {
+  state.panels = [{
+    title: '월요일',
+    description: '등록 시간: 0시간'
+  },{
+    title: '화요일',
+    description: '등록 시간: 0시간'
+  },{
+    title: '수요일',
+    description: '등록 시간: 10시간'
+  }];
+
+  render();
+}, 1000);
+
+render();
